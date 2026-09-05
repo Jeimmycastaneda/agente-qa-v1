f8b40b94fe6b6251dd9d304afe446a80f5d07f64
Jeimmycastaneda <125413926+Jeimmycastaneda@users.noreply.github.com>
2026-09-04 19:10:49 -0500

    Refactor: incorporar defaults a estructura objetivo

---
+++ agente_qa/defaults.py
@@ -0,0 +1,7 @@
+"""Valores predeterminados de la aplicación modular."""
+from agente_qa.config import APP_VERSION, DEFAULT_PROVIDER, FALLBACK_MODELS
+
+DEFAULT_PRODUCT = "Cotizadores Web"
+DEFAULT_MODULE = "Cotizador Autos Colectivos"
+DEFAULT_AREA_PATH = "COTIZADORES WEB\\DESARROLLO"
+DEFAULT_TIPO_ORIGEN_PROYECTO = "Proyecto"
